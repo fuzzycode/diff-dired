ba21fd3f61b49198a7031226308d62abb1b44268
Björn Larsson <b.e.larsson@gmail.com>
2023-01-02 20:41:14 +0100

    Add package skeleton

---
+++ diff-dired.el
@@ -0,0 +1,25 @@
+;;; diff-dired.el --- A Dired Utility to list changed files -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2023 Björn Larsson
+;;
+;; Author: Björn Larsson <develop@bjornlarsson.net>
+;; Maintainer: Björn Larsson <develop@bjornlarsson.net>
+;; Created: January 02, 2023
+;; Modified: January 02, 2023
+;; Version: 0.0.1
+;; Keywords: convenience extensions files vc
+;; Homepage: https://github.com/fuzzycode/diff-dired
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;; Code:
+
+
+
+(provide 'diff-dired)
+;;; diff-dired.el ends here
